4e0b57283c4618ef40846efc642a6697f8b2b8de
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-17 12:39:00 +0200

    feat: define renderBurger function

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function init() {
+const MY_MENU = document.getElementById('menu-content');
 
+function init() {
+    renderBurger();
+}
+
+function renderBurger() {
+    let myBurgerList = "";
+    for (let i = 0; i < products.burger.length; i++) {
+        myBurgerList += templateBurgerMenu(i);
+    }
+    return myBurgerList;
 }
