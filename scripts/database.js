fecec66a44cc7937a25e20bf740a82f049baadc6
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-17 10:54:53 +0200

    add: JSON-array with burgers

---
+++ scripts/database.js
@@ -0,0 +1,30 @@
+let products = [
+    {
+        "burger": [
+            {
+                "name": "Veggie mushroom black burger",
+                "ingredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+                "price": 16.90,
+                "image": "./assets/image/Veggie_mushroom_black_burger.png"
+            },
+            {
+                "name": "All meat burger",
+                "ingredients": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+                "price": 15.90,
+                "image": "./assets/image/All_meat_burger.png"
+            },
+            {
+                "name": "Beef red burger",
+                "ingredients": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+                "price": 14.90,
+                "image": "./assets/image/Beef_red_burger.png"
+            },
+            {
+                "name": "Big chicken burger",
+                "ingredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+                "price": 15.90,
+                "image": "./assets/image/Big_chicken_burger.png"
+            },
+        ]
+    },
+]
