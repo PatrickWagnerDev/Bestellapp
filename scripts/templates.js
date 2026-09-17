edc8c60dd9f18648cfc049bafdee8976d147166c
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-17 12:51:19 +0200

    feat: fill JSON-Inserts in templateBurgerMenu

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,19 +1,19 @@
 function templateBurgerMenu(i) {
     return /*html*/`
         <article class="menu-part">
-            <img src="./assets/image/Veggie_mushroom_black_burger.png" alt="Veggie mushroom black burger">
+            <img src="${products.burger[i].image}" alt="Veggie mushroom black burger">
             <section class="menu-description">
                 <article>
                     <h3>
-                        Veggie mushroom black burger
+                        ${products.burger[i].name}
                     </h3>
                     <p>
-                        Mixed green salad, Tomatoes, Edamame, Mushrooms
+                        ${products.burger[i].ingredients}
                     </p>
                 </article>
                 <article class="menu-price">
                     <p>
-                        16,90€
+                        ${products.burger[i].price.toFixed(2).replace(".", ",")}€
                     </p>
                     <button>
                         Add to basket
