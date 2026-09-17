5a1c70b06f0400b18c10bd7683472cfa3a61f977
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-17 12:46:57 +0200

    feat: define templateBurgerMenu structure

---
+++ scripts/templates.js
@@ -0,0 +1,25 @@
+function templateBurgerMenu(i) {
+    return /*html*/`
+        <article class="menu-part">
+            <img src="./assets/image/Veggie_mushroom_black_burger.png" alt="Veggie mushroom black burger">
+            <section class="menu-description">
+                <article>
+                    <h3>
+                        Veggie mushroom black burger
+                    </h3>
+                    <p>
+                        Mixed green salad, Tomatoes, Edamame, Mushrooms
+                    </p>
+                </article>
+                <article class="menu-price">
+                    <p>
+                        16,90€
+                    </p>
+                    <button>
+                        Add to basket
+                    </button>
+                </article>
+            </section>
+        </article>
+    `;
+}
